b18d832f37d903d6b00655cb2f03bfdadd82738f
mrgokulnathgm03@gmail.com <mrgokulnathg03@gmail.com>
2026-07-17 20:29:04 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def detail(request):
+    return render(request, 'detail.html')
